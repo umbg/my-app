277d3a897982ef2d1d856e811e455ed337ba7beb
Umberto Green <umbg.5295.gt@gmail.com>
2022-02-01 22:09:57 +0900

    Successfully published via angular-cli-ghpages! Have a nice day!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserService } from './user/user.service';
 import { UserListComponent } from './user/user-list/user-list.component';
 import { UserEditComponent } from './user/user-edit/user-edit.component';
+import { ConfirmDialogComponent } from './tools/confirm-dialog/confirm-dialog.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +21,22 @@ import { UserEditComponent } from './user/user-edit/user-edit.component';
     MenuComponent,
     DashboardComponent,
     UserListComponent,
-    UserEditComponent
+    UserEditComponent,
+    ConfirmDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatDialogModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    UserService
+  ],
+  entryComponents: [
+    ConfirmDialogComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/user/user-edit/user-edit.component.ts
+++ src/app/user/user-edit/user-edit.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../../tools/confirm-dialog/confirm-dialog.component';
+
+import { User } from '../user';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-edit',
@@ -6,10 +12,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-edit.component.css']
 })
 export class UserEditComponent implements OnInit {
+  user: User = { id: 0, name: '', email: '' };
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: UserService,
+    private dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    var user = this.service.getUser(id);
+    if (user) {
+      this.user = user;
+    }
+  }
+
+  onSubmit(form: any): void {
+    let user = {
+      id: form.id,
+      name: form.name,
+      email: form.email
+    };
+    let dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      width: '300px',
+      data: {
+        title: 'Confirm',
+        message: 'Are you sure you want to update this user?'
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === true) {
+        this.service.setUser(user);
+        this.router.navigate(["/users"]);
+      }
+    });
   }
 
 }
